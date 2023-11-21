478cddfce0b62bbac461b270f9f2e92d78984f87
ami <ami@Ubuntu.myguest.virtualbox.org>
2023-11-21 13:37:17 -0600

    Works with small sample testing with big sample.

--- src/day1/1.go
+++ src/day1/1.go
@@ -14,19 +14,33 @@ func main() {
 }
 
 
-
 func sum() int {
     calibration_values := 0 
-    scanner := bufio.NewScanner(os.Stdin)
+    data, err := os.Open("1_small_sample.txt")
+    if err != nil {
+        panic(err)
+    }
+    scanner := bufio.NewScanner(data)
+    
     for scanner.Scan() {
         line := scanner.Text()
-        fmt.Println(line)
+        start, end := 0, 0
+        runeArr := []rune(line) 
+        for j := 0; j < len(runeArr) ; j++ {
+            if int(runeArr[j]-'0') > 0 && int(runeArr[j]-'0') < 9 {
+                if i := int(runeArr[j]-'0') ; start == 0 {
+                    start = i 
+                    end = i
+                } else {
+                    end = i
+                }
+            }
+        }
+        calibration_values += (start*10)+end
     }
-
     if err := scanner.Err(); err != nil {
         fmt.Fprintln(os.Stderr, "reading standard input:", err)
     }
     
-    fmt.Println("Hi")    
     return calibration_values;
 }
