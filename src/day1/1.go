b9ed9d0a8a6c661e54079fef341c02e793ab1651
ami <ami@Ubuntu.myguest.virtualbox.org>
2023-11-21 12:34:37 -0600

    Setting up go file for first time, trying to read file input

---
+++ src/day1/1.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+
+
+func main() {
+ 
+    fmt.Println(sum())
+}
+
+
+
+func sum() int {
+    calibration_values := 0 
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        line := scanner.Text()
+        fmt.Println(line)
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "reading standard input:", err)
+    }
+    
+    
+    return calibration_values;
+}
